abb0baf25ea6a4a22db251ecca24d6ab7192ed5b
Lasse S. Haslev <lasse@haslev.no>
2016-07-07 12:55:29 +0200

    Now this is working

--- src/main.js
+++ src/main.js
@@ -1,28 +1,26 @@
 import Vue from 'vue'
 import ImagePicker from './ImagePicker/ImagePicker.vue'
 
+var data = [];
+for (var i = 0, len = 10; i < len; i++) {
+    data.push({
+        id: i,
+        path: "http://placehold.it/350x350",
+    });
+}
+
 new Vue({
     el: 'body',
 
     data() {
         return {
-            images: [
-                {
-                    path: "http://placehold.it/350x350",
-                },
-                {
-                    path: "http://placehold.it/350x350",
-                },
-                {
-                    path: "http://placehold.it/350x350",
-                },
-                {
-                    path: "http://placehold.it/350x350",
-                },
-                {
-                    path: "http://placehold.it/350x350",
-                },
-            ]
+            images: data,
+        }
+    },
+
+    events: {
+        'ImageSelected'( selected ) {
+            console.log( selected );
         }
     },
 
